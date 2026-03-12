46d8c947433fe5a7ba45774fef57a407845a01b4
mardriel-commits <anselmonunes2@gmail.com>
2026-03-11 23:54:23 -0300

    Delete controledecontratos directory

--- controledecontratos/backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .company import Company
-from .contract import Contract
-from .saldo_movement import SaldoMovement
-from .contract_action import ContractAction
-from .alert_log import AlertLog
