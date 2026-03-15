3a083fb518dfb154498c00bb68898677864c952a
mardriel-commits <anselmonunes2@gmail.com>
2026-03-15 20:44:07 -0300

    Update PageLoader.jsx

--- frontend/src/components/PageLoader.jsx
+++ frontend/src/components/PageLoader.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
 
+export default function PageLoader({
+  title = 'Carregando informações',
+  subtitle = 'Aguarde um instante.',
+  minHeight = 220,
+}) {
+  return (
+    <div className="card" style={{ minHeight }}>
+      <div className="loading-block">
+        <div className="spinner" />
+        <div style={{ fontWeight: 900 }}>{title}</div>
+        <div className="small">{subtitle}</div>
+      </div>
+    </div>
+  )
+}
